3ffdaa84da44944eb5557a32c53c2542b76b0c3a
Chris Conley <pair+chris@realscout.com>
2016-01-06 10:47:59 -0800

    Removes brew-cask installation

--- recipes/cask.rb
+++ recipes/cask.rb
@@ -18,16 +18,6 @@
 #
 Chef::Resource.send(:include, Homebrew::Mixin)
 
-homebrew_tap 'caskroom/cask'
-
-package 'brew-cask'
-
-execute 'update homebrew cask from github' do
-  user node['homebrew']['owner'] || homebrew_owner
-  command '/usr/local/bin/brew upgrade brew-cask && /usr/local/bin/brew cask cleanup || true'
-  only_if { node['homebrew']['auto-update'] }
-end
-
 directory '/opt/homebrew-cask' do
   owner node['homebrew']['owner'] || homebrew_owner
   mode 00775
